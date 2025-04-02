221c0dbf6d757cad4279b74e26acd6fd56759237
nathen-win <rpriyo.uic.edu>
2025-04-02 13:30:25 -0500

    Added geolocation features

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import ErrorPage from './ErrorPage.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -13,19 +14,27 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/placess');
+        const response = await fetch('http://localhost:3000/places');
         const resData = await response.json();
 
         if (!response.ok) { // 400, 500
           throw new Error(`Failed to fetch places`);
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places, 
+            position.coords.latitude, 
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+
       } catch(error) {
         setError({message: error.message || 'Could not fetch places, please try again later.'});
+        setIsFetching(false);
       }
-      
-      setIsFetching(false);
     }
 
     fetchPlaces();
